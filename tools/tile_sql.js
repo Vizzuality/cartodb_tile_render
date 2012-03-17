0a7f93a382c8498cb497f6a8c5c8b70d08994e2a
Sandro Santilli <strk@keybit.net>
2012-03-17 10:02:21 +0100

    Allow omitting Y or X to scan all line or area in a single zoom

--- tools/tile_sql.js
+++ tools/tile_sql.js
@@ -1,8 +1,6 @@
 var MercatorProjection = require('../gmaps_mercator').MercatorProjection;
 var CartoDB = require('../cartodb_vector').CartoDB;
 
-console.log('x: ' + typeof(CartoDB));
-
 function data_for_tile(table, x, y, z, callback) {
     var opts = this.options;
     var prj = new MercatorProjection();
@@ -11,13 +9,54 @@ function data_for_tile(table, x, y, z, callback) {
     callback(sql);
 };
 
+function parse_range(val) {
+  var p = val.split(':');
+  return p;
+};
 
-if (process.argv.length == 6) {
-    args = process.argv.slice(2)
-    data_for_tile(args[0], parseInt(args[1]), parseInt(args[2]), parseInt(args[3]), function(sql) {
-        console.log(sql);
-    });
+
+if (process.argv.length < 4) {
+   console.log("usage: node " + process.argv[1] + " <table> <zoom> [<x>] [<y>]")
 } else {
-    console.log("usage: node " + process.argv[1] + " table x y zoom")
-}
+
+   args = process.argv.slice(2)
+   var table = args[0];
+
+   var zoom = parseInt(args[1]);
+   var ntiles = 1<<zoom;
+
+   var miny = 0;
+   var maxy = ntiles-1;
+
+   var minx = 0;
+   var maxx = ntiles-1;
+
+   if ( args.length > 2 ) {
+    range = args[2].split(':');
+    miny = parseInt(range[0]);
+    if ( range.length > 1 ) maxy = parseInt(range[1]);
+    else maxy = miny;
+   }
+
+   if ( args.length > 3 ) {
+    range = args[3].split(':');
+    minx = parseInt(range[0]);
+    if ( range.length > 1 ) maxx = parseInt(range[1]);
+    else maxx = minx;
+   }
+
+   var logger = function(sql) {
+        console.log(sql);
+   };
+
+  console.log("-- Y range = " + miny + ":" + maxy);
+  console.log("-- X range =  " + minx + ":" + maxx);
+
+  for (y=miny; y<=maxy; ++y) {
+    for (x=minx; x<=maxx; ++x) {
+      data_for_tile(table, x, y, zoom, logger);
+    }
+  }
+
+} 
 
